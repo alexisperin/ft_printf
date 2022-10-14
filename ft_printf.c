e49026bce7af4da8a98128634bfb90a7a140db2c
Alexis Perin <alexis.perin@hotmail.com>
2022-10-14 09:24:38 +0200

    Basic structure done

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,44 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 08:48:30 by aperin            #+#    #+#             */
-/*   Updated: 2022/10/13 16:34:07 by aperin           ###   ########.fr       */
+/*   Updated: 2022/10/14 09:20:32 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *str, ...)
 {
+	int			i;
+	va_list		ap;
+	t_config	config;
+
+	i = 0;
+	config.len = 0;
+	va_start(ap, str);
+	while (str[i])
+	{
+		while (str[i] && str[i] != '%')
+		{
+			write(1, &str[i], 1);
+			config.len++;
+			i++;
+		}
+		if (str[i] == '%')
+		{
+			parse(&str[i], ap, &config);
+			print_config(config);
+			i += config.i;
+		}
+	}
+	va_end(ap);
+	return (config.len);
+}
+
+void	parse(char *str, va_list ap, t_config *config)
+{
+	config->i = 1;
+
+	if (str[config->i] == '%')
+		config->conversion = '%';
 }
--- main.c
+++ main.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/12 09:02:23 by aperin            #+#    #+#             */
-/*   Updated: 2022/10/14 09:16:16 by aperin           ###   ########.fr       */
+/*   Created: 2022/10/13 17:42:05 by aperin            #+#    #+#             */
+/*   Updated: 2022/10/14 09:21:33 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include <stdio.h>
 
-# include <unistd.h>
-# include <stdarg.h>
-
-typedef struct s_config
+int	main(void)
 {
-	char	conversion;
-	void	*content;
-	int		i;
-	int		len;
-}			t_config;
-
-int	ft_printf(const char *format, ...);
-
-#endif
+	printf("%%%");
+}
